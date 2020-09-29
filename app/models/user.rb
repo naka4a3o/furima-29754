d0e296dfc13b1e147f521514df82ed6ef28243c2
naka4a3o <momokoro0123n@icloud.com>
2020-09-29 18:29:58 +0900

    ActiveHashに関わるモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,7 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
   validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+
+  has_many :items
+
 end
---
+++ spec/factories/shipping_bearers.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :shipping_bearer do
+    
+  end
+end

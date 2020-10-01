53af6b3350b3598add4c41f26c77eeb1810bfa91
naka4a3o <momokoro0123n@icloud.com>
2020-10-01 21:00:08 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,13 +10,13 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params) 
+    @item = Item.new(item_params)
 
     if @item.valid?
       @item.save
       redirect_to root_path
     else
-      #@items = Item.includes(:user)
+      # @items = Item.includes(:user)
       render :new
     end
   end

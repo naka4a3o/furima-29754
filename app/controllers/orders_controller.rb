ee5c4139cbcfa5e5801c1c6bcffa856b897425dd
naka4a3o <momokoro0123n@icloud.com>
2020-10-06 18:52:37 +0900

    遷移先の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,36 +1,42 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :move_to_index, only: :edit
+  before_action :move_to_index, only: :index
   def index
     @order = UserOrder.new
     @item = Item.find(params[:item_id])
+
     # @orders = @item.orders.includes(:user)
   end
 
-  def create
-    @order = UserOrder.new(order_params)
+  def create    
     @item = Item.find(params[:item_id])
+    @order = UserOrder.new(order_params)
     if @order.valid?
-      Payjp.api_key = "sk_test_3ce1ae63516885802ee8c08a" 
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],  
-        currency: 'jpy'               
-      )
+      pay_item
       @order.save
       redirect_to root_path
     else
       render :index
     end
   end
-
+  
   private
 
   def order_params
     params.require(:user_order).permit(:postal_code, :item_prefecture_id, :city, :house_number, :building_name, :phone_number, :token).merge(item_id: params[:item_id], user_id: current_user.id)
   end
 
-  def move_to_show
-    redirect_to root_path if user_signed_in? && current_user.id == @item.user_id
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if user_signed_in? && current_user.id == @item.user_id && Order.exists?(item_id: @item.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],  
+      currency: 'jpy'
+    )
   end
 end

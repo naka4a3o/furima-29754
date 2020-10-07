70bd592665d611b3ba6dd2f0aa4185b263f3026d
naka4a3o <momokoro0123n@icloud.com>
2020-10-07 16:03:36 +0900

    修正2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,7 +29,7 @@ class OrdersController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:item_id])
-    redirect_to root_path if user_signed_in? && current_user.id == @item.user_id && Order.exists?(item_id: @item.id)
+    redirect_to root_path if current_user.id == @item.user_id || Order.exists?(item_id: @item.id)
   end
 
   def pay_item
--- spec/models/user_order_spec.rb
+++ spec/models/user_order_spec.rb
@@ -9,53 +9,59 @@ RSpec.describe UserOrder, type: :model do
       sleep(1)
     end
 
-    it 'すべての値が正しく入力されていれば保存できること' do
-      expect(@user_order).to be_valid
+    context '保存がうまくいくとき' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@user_order).to be_valid
+      end
     end
-    it 'postal_codeが空だと保存できないこと' do
-      @user_order.postal_code = nil
-      @user_order.valid?
-      expect(@user_order.errors.full_messages).to include("Postal code can't be blank")
-    end
-    it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
-      @user_order.postal_code = '1234567'
-      @user_order.valid?
-      expect(@user_order.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
-    end
-    it 'item_prefecture_idを選択していないと保存できないこと' do
-      @user_order.item_prefecture_id = 0
-      @user_order.valid?
-      expect(@user_order.errors.full_messages).to include("Item prefecture can't be blank")
-    end
-    it 'cityが空だと保存できないこと' do
-      @user_order.city = nil
-      @user_order.valid?
 
-      expect(@user_order.errors.full_messages).to include("City can't be blank")
-    end
-    it 'house_numberは空では保存できないこと' do
-      @user_order.house_number = nil
-      @user_order.valid?
-      expect(@user_order.errors.full_messages).to include("House number can't be blank")
-    end
-    it 'building_nameは空でも保存できること' do
-      @user_order.building_name = nil
-      expect(@user_order).to be_valid
-    end
-    it 'phone_numberが空だと保存できないこと' do
-      @user_order.phone_number = nil
-      @user_order.valid?
-      expect(@user_order.errors.full_messages).to include("Phone number can't be blank")
-    end
-    it 'phone_numberはハイフンは不要で、11桁を超過すると保存できないこと' do
-      @user_order.phone_number = '090-1234-5678'
-      @user_order.valid?
-      expect(@user_order.errors.full_messages).to include('Phone number is not a number')
-    end
-    it 'tokenが空では登録できないこと' do
-      @user_order.token = nil
-      @user_order.valid?
-      expect(@user_order.errors.full_messages).to include("Token can't be blank")
+
+    context '保存がうまくいかないとき' do 
+      it 'postal_codeが空だと保存できないこと' do
+        @user_order.postal_code = nil
+        @user_order.valid?
+        expect(@user_order.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @user_order.postal_code = '1234567'
+        @user_order.valid?
+        expect(@user_order.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+      end
+      it 'item_prefecture_idを選択していないと保存できないこと' do
+        @user_order.item_prefecture_id = 0
+        @user_order.valid?
+        expect(@user_order.errors.full_messages).to include("Item prefecture can't be blank")
+      end
+      it 'cityが空だと保存できないこと' do
+        @user_order.city = nil
+        @user_order.valid?
+
+        expect(@user_order.errors.full_messages).to include("City can't be blank")
+      end
+      it 'house_numberは空では保存できないこと' do
+        @user_order.house_number = nil
+        @user_order.valid?
+        expect(@user_order.errors.full_messages).to include("House number can't be blank")
+      end
+      it 'building_nameは空でも保存できること' do
+        @user_order.building_name = nil
+        expect(@user_order).to be_valid
+      end
+      it 'phone_numberが空だと保存できないこと' do
+        @user_order.phone_number = nil
+        @user_order.valid?
+        expect(@user_order.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_numberはハイフンは不要で、11桁を超過すると保存できないこと' do
+        @user_order.phone_number = '090-1234-5678'
+        @user_order.valid?
+        expect(@user_order.errors.full_messages).to include('Phone number is not a number')
+      end
+      it 'tokenが空では登録できないこと' do
+        @user_order.token = nil
+        @user_order.valid?
+        expect(@user_order.errors.full_messages).to include("Token can't be blank")
+      end
     end
   end
 end
